ab37f87d7c1860549bc0e3c70797a266f531a55e
Kraiviks <kraiviks@gmail.com>
2022-02-07 20:01:37 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 	Col,
 	Input,
 	Form,
+	ListGroup,
+	ListGroupItem,
 } from "reactstrap";
 
 function App() {
@@ -84,7 +86,7 @@ function App() {
 					<TabContent activeTab={activeTab}>
 						<TabPane tabId="1">
 							<Row>
-								<Col sm="12" className="inputs_wrapp">
+								<Col sm="12" className="inputs_wrapp mt-5">
 									<label htmlFor="inputHeader">
 										Заголовок:
 										<Input
@@ -93,12 +95,15 @@ function App() {
 											placeholder="Header"
 											type="text"
 											required
+											invalid={
+												header !== "" ? false : true
+											}
 											onChange={(e) => {
 												setHeader(e.target.value);
 											}}
 										/>
 										<div className="error">
-											{headerError}
+											{header === "" ? headerError : ""}
 										</div>
 									</label>
 									<label htmlFor="inputTextarea">
@@ -109,7 +114,10 @@ function App() {
 											type="textarea"
 										/>
 									</label>
-									<label htmlFor="status">
+									<label
+										htmlFor="status"
+										className="mt-3 mb-3"
+									>
 										Статус: on|off
 										<Input
 											id="status"
@@ -135,7 +143,7 @@ function App() {
 						</TabPane>
 						<TabPane tabId="2">
 							<Row>
-								<Col sm="12" className="inputs_wrapp">
+								<Col sm="12" className="inputs_wrapp mt-5">
 									<label htmlFor="tel">
 										Телефон:
 										<Input
@@ -143,12 +151,15 @@ function App() {
 											name="tel"
 											type="text"
 											required
+											invalid={
+												phone !== "" ? false : true
+											}
 											onChange={(e) => {
 												setPhone(e.target.value);
 											}}
 										/>
 										<div className="error">
-											{phoneError}
+											{phone === "" ? phoneError : ""}
 										</div>
 									</label>
 									<label htmlFor="tel">
@@ -185,7 +196,7 @@ function App() {
 						</TabPane>
 						<TabPane tabId="3">
 							<Row>
-								<Col sm="12" className="inputs_wrapp">
+								<Col sm="12" className="inputs_wrapp mt-5">
 									<label htmlFor="photo">
 										Photos: выберите до 5 фото
 										<Input
@@ -218,47 +229,60 @@ function App() {
 						</TabPane>
 						<TabPane tabId="4">
 							<Row>
-								<Col sm="12" className="inputs_wrapp">
-									<label htmlFor="services1">
-										Paid services 1
-										<Input
-											id="services1"
-											name="services1"
-											type="checkbox"
-										/>
-									</label>
-									<label htmlFor="services2">
-										Paid services 2
-										<Input
-											id="services2"
-											name="services2"
-											type="checkbox"
-										/>
-									</label>
-									<label htmlFor="services3">
-										Paid services 3
-										<Input
-											id="services3"
-											name="services3"
-											type="checkbox"
-										/>
-									</label>
-									<label htmlFor="services4">
-										Paid services 4
-										<Input
-											id="services4"
-											name="services4"
-											type="checkbox"
-										/>
-									</label>
-									<label htmlFor="services5">
-										Paid services 5
-										<Input
-											id="services5"
-											name="services5"
-											type="checkbox"
-										/>
-									</label>
+								<Col sm="12" className="inputs_wrapp mt-5">
+									<ListGroup>
+										<ListGroupItem>
+											<label htmlFor="services1">
+												Paid services 1:
+											</label>
+											<Input
+												id="services1"
+												name="services1"
+												type="checkbox"
+											/>
+										</ListGroupItem>
+										<ListGroupItem>
+											<label htmlFor="services2">
+												Paid services 2:
+											</label>
+											<Input
+												id="services2"
+												name="services2"
+												type="checkbox"
+											/>
+										</ListGroupItem>
+										<ListGroupItem>
+											<label htmlFor="services3">
+												Paid services 3:
+											</label>
+											<Input
+												id="services3"
+												name="services3"
+												type="checkbox"
+											/>
+										</ListGroupItem>
+										<ListGroupItem>
+											<label htmlFor="services4">
+												Paid services 4:
+											</label>
+											<Input
+												id="services4"
+												name="services4"
+												type="checkbox"
+											/>
+										</ListGroupItem>
+										<ListGroupItem>
+											<label htmlFor="services5">
+												Paid services 5:
+											</label>
+											<Input
+												id="services5"
+												name="services5"
+												type="checkbox"
+											/>
+										</ListGroupItem>
+									</ListGroup>
+
 									<div className="buttons">
 										<Button
 											color="primary"
@@ -271,13 +295,15 @@ function App() {
 										</Button>
 									</div>
 									<div className="message-box">{message}</div>
-									<ul>
+									<ListGroup>
 										{formD
 											? formD.map((item, index) => (
-													<li key={index}>{item}</li>
+													<ListGroupItem key={index}>
+														{item}
+													</ListGroupItem>
 											  ))
 											: null}
-									</ul>
+									</ListGroup>
 								</Col>
 							</Row>
 						</TabPane>
